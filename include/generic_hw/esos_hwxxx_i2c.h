543eeaff188546cf7acdfca99fb6de11e583cc40
J.W.Bruce <jwbruce@tntech.edu>
2019-04-16 15:30:27 -0500

    updated the generic_hwxxx i2c files to match the new simpler I2C service structure.

--- include/generic_hw/esos_hwxxx_i2c.h
+++ include/generic_hw/esos_hwxxx_i2c.h
@@ -25,8 +25,8 @@
  *
  */
 
-#ifndef _ESOS_hwxxx_I2C_H
-#define _ESOS_hwxxx_I2C_H
+#ifndef _ESOS_HWXXX_I2C_H
+#define _ESOS_HWXXX_I2C_H
 
 
 /**
@@ -37,176 +37,119 @@
 // Documentation for this file. If the \file tag isn't present,
 // this file won't be documented.
 /** \file
-*  \brief This file contains routines which define, configure and
-*  allow use of the I<sup>2</sup>C service on the target
-* hardware (hwxxxx)
+*  \brief This file contains template routines which define,
+* configure and allow use of the I<sup>2</sup>C service on
+* some specific hardware
 */
 
 
 /* I N C L U D E S **********************************************************/
 #include "esos.h"
+#include "esos_i2c.h"
 #include "esos_hwxxx.h"
 
-
 /* D E F I N I T I O N S ****************************************************/
-#ifndef I2C_ACK
-#define I2C_ACK 0
-#endif
-#ifndef I2C_NAK
-#define I2C_NAK 1
-#endif
 
 /* E X T E R N S ************************************************************/
 extern struct stTask    __stChildTaskI2C, __stGrandChildTaskI2C;
 extern uint8_t            __esos_i2c_dataBytes[2];                    // used to store arguments
 
 /* M A C R O S **************************************************************/
-#define I2C_WADDR(x) (x & 0xFE) //clear R/W bit of I2C addr
-#define I2C_RADDR(x) (x | 0x01) //set R/W bit of I2C addr
-
-#define ESOS_TASK_WAIT_ON_AVAILABLE_I2C()                                           \
-        do {                                                                        \
-        ESOS_TASK_WAIT_WHILE(__esos_IsSystemFlagSet(__ESOS_SYS_I2C_IS_BUSY));   \
-        __esos_SetSystemFlag(__ESOS_SYS_I2C_IS_BUSY);                           \
-    } while(0)
-
-#define ESOS_TASK_SIGNAL_AVAILABLE_I2C() __esos_ClearSystemFlag(__ESOS_SYS_I2C_IS_BUSY)
-
-// Macros to perform I2C operations within a child task
-#define __HWXXX_I2C1_START()                   \
-   do{                                         \
-      I2C1CONbits.SEN = 1;                     \
-      ESOS_TASK_WAIT_WHILE( I2C1CONbits.SEN);  \
-   }while(0)
-
-#define __HWXXX_I2C1_RSTART()                \
-  do{                                        \
-    I2C1CONbits.RSEN = 1;                    \
-    ESOS_TASK_WAIT_WHILE( I2C1CONbits.RSEN); \
-  }while(0)
-
-#define __HWXXX_I2C1_STOP()                  \
-  do{                                        \
-    I2C1CONbits.PEN = 1;                     \
-    ESOS_TASK_WAIT_WHILE( I2C1CONbits.PEN);  \
-  }while(0)
-
-#define __HWXXX_I2C1_PUT(byte)                   \
-  do{                                            \
-    I2C1TRN = (byte);                            \
-    ESOS_TASK_WAIT_WHILE( I2C1STATbits.TRSTAT);  \
-  }while(0)
-
-#define   ESOS_TASK_WAIT_ON_GETI2C1( pu8_get, u8_ack2Send )                     \
-            ESOS_TASK_SPAWN_AND_WAIT( (ESOS_TASK_HANDLE)&__stGrandChildTaskI2C, __esos_hwxxx_getI2C1, (pu8_get), (u8_ack2Send) )
-
-// Must copy value to a var, send &var, just in case u8_d1 is a literal
-/**
-Transaction: Write 1 (ONE) byte stored in variable \em u8_d1 to I2C slave at address \em u8_addr.
-\param u8_addr  Slave I2C address
-\param u8_d1   Variable containing first byte to write
-\sa ESOS_TASK_WAIT_ON_WRITE2I2C1
-\sa ESOS_TASK_WAIT_ON_WRITENI2C1
-\sa esos_hwxxx_configI2C1
-\hideinitializer
-*/
-#define   ESOS_TASK_WAIT_ON_WRITE1I2C1( u8_addr, u8_d1 )                     \
-    do{                                                                        \
-      __esos_i2c_dataBytes[0] = (u8_d1);                                      \
-      ESOS_TASK_SPAWN_AND_WAIT( (ESOS_TASK_HANDLE)&__stChildTaskI2C, __esos_hwxxx_writeNI2C1, (u8_addr), &__esos_i2c_dataBytes[0], 1 );  \
-    }while(0)
-
-// We needed a 2-byte array to be able to use the buffer.
-// Otherwise, we'd need two writeN calls, which require
-// child tasks, i.e. code size grows
+#define	CLEAR_REGISTER_BITS(reg,mask)			reg &= (~(mask))
+#define	SET_REGISTER_BITS(reg,mask)				reg |= (mask)
+#define	MAKE_I2C_WR_ADDR(bits)					bits &= 0xFFE
+#define	MAKE_I2C_RD_ADDR(bits)					bits &= 0x001
 
 /**
-Transaction: Write 2 (TWO) bytes stored in variables \em u8_d1 and \em u8_d2 to I2C slave at address \em u8_addr.
-\param u8_addr  Slave I2C address
-\param u8_d1   Variable containing first byte to write
-\param u8_d2   Variable containing second byte to write
-\sa ESOS_TASK_WAIT_ON_WRITE1I2C1
-\sa ESOS_TASK_WAIT_ON_WRITENI2C1
-\hideinitializer
-*/
-#define   ESOS_TASK_WAIT_ON_WRITE2I2C1( u8_addr, u8_d1, u8_d2 )              \
+ * Transaction: Write 2 (TWO) bytes stored in variables \em u8_d1 and \em u8_d2 to I2C slave at address \em u8_addr.
+ * \param u8_addr  Slave I2C address
+ * \param u8_d1   Variable containing first byte to write
+ * \param u8_d2   Variable containing second byte to write
+ * \sa ESOS_TASK_WAIT_ON_WRITE1I2C
+ * \sa ESOS_TASK_WAIT_ON_WRITENI2C
+ * \hideinitializer
+ * 
+ * */
+#define   ESOS_TASK_WAIT_ON_WRITE2I2C( u8_addr, u8_d1, u8_d2 )              \
     do{                                                                        \
       __esos_i2c_dataBytes[0] = (u8_d1);                                      \
       __esos_i2c_dataBytes[1] = (u8_d2);                                      \
-      ESOS_TASK_SPAWN_AND_WAIT( (ESOS_TASK_HANDLE)&__stChildTaskI2C, __esos_hwxxx_writeNI2C1, (u8_addr), &__esos_i2c_dataBytes[0], 2 );    \
+      ESOS_TASK_SPAWN_AND_WAIT( (ESOS_TASK_HANDLE)&__stChildTaskI2C, __esos_i2c_hw_writeN, (u8_addr), &__esos_i2c_dataBytes[0], 2 );    \
     }while(0)
 
 /**
-Transaction: Write \em u16_cnt bytes stored in buffer \em *pu8_d to I2C slave at address \em u8_addr.
-\param u8_addr  Slave I2C address
-\param pu8_d Pointer to buffer containing bytes to send
-\param u16_cnt Number of bytes to send
-\sa ESOS_TASK_WAIT_ON_WRITE1I2C1
-\sa ESOS_TASK_WAIT_ON_WRITE2I2C1
-\hideinitializer
-*/
-#define   ESOS_TASK_WAIT_ON_WRITENI2C1( u8_addr, pu8_d, u16_cnt )              \
-            ESOS_TASK_SPAWN_AND_WAIT( (ESOS_TASK_HANDLE)&__stChildTaskI2C, __esos_hwxxx_writeNI2C1, (u8_addr), (pu8_d), (u16_cnt) )
+ * Transaction: Write \em u8_cnt bytes stored in buffer \em *pu8_d to I2C slave at address \em u8_addr.
+ * \param u8_addr  Slave I2C address
+ * \param pu8_d Pointer to buffer containing bytes to send
+ * \param u8_cnt Number of bytes to send
+ * \sa ESOS_TASK_WAIT_ON_WRITE1I2C
+ * \sa ESOS_TASK_WAIT_ON_WRITE2I2C
+ * \hideinitializer
+ * 
+ * */
+#define   ESOS_TASK_WAIT_ON_WRITENI2C( u8_addr, pu8_d, u8_cnt )              \
+            ESOS_TASK_SPAWN_AND_WAIT( (ESOS_TASK_HANDLE)&__stChildTaskI2C, __esos_i2c_hw_writeN, (u8_addr), (pu8_d), (u8_cnt) )
 
 /**
-Transaction: Read 1 (ONE) byte from I2C slave at address \em u8_addr, and save to variable \em u8_d1
-As per the I2C standard, a NAK is returned for the last byte read from the slave, ACKs are returned for the other bytes.
-\param u8_addr  Slave I2C address
-\param u8_d1   variable to hold the read byte
-\hideinitializer
-\sa ESOS_TASK_WAIT_ON_READ2I2C1
-\sa ESOS_TASK_WAIT_ON_READNI2C1
-\sa esos_hwxxx_configI2C1
-*/
-#define   ESOS_TASK_WAIT_ON_READ1I2C1( u8_addr, u8_d1 )                     \
-            ESOS_TASK_SPAWN_AND_WAIT( (ESOS_TASK_HANDLE)&__stChildTaskI2C, __esos_hwxxx_readNI2C1, (u8_addr), &(u8_d1), 1 )
+ * Transaction: Read 1 (ONE) byte from I2C slave at address \em u8_addr, and save to variable \em u8_d1
+ * As per the I2C standard, a NAK is returned for the last byte read from the slave, ACKs are returned for the other bytes.
+ * \param u8_addr  Slave I2C address
+ * \param u8_d1   variable to hold the read byte
+ * \hideinitializer
+ * \sa ESOS_TASK_WAIT_ON_READ2I2C
+ * \sa ESOS_TASK_WAIT_ON_READNI2C
+ * \sa esos_hwxxx_configI2C1
+ * 
+ * */
+#define   ESOS_TASK_WAIT_ON_READ1I2C( u8_addr, u8_d1 )                     \
+            ESOS_TASK_SPAWN_AND_WAIT( (ESOS_TASK_HANDLE)&__stChildTaskI2C, __esos_i2c_hw_readN, (u8_addr), &(u8_d1), 1 )
 
 // While READ1 still worked fine, READ2 needs a new trick, since u8_d1 and u8_d2 are
 // not in consecutive memory locations.  Can't do two READN calls, because that (again)
 // requires another child task.  Instead, just re-use our 2-byte array as a read buffer
-
 /**
-Transaction: Read 2 (TWO) bytes from I2C slave at address \em u8_addr, save to variables \em u8_d1 and \em u8_d2
-As per the I2C standard, a NAK is returned for the last byte read from the slave, ACKs are returned for the other bytes.
-\param u8_addr  Slave I2C address
-\param u8_d1   variable to hold first read byte
-\param u8_d2   variable to hold second read byte
-\hideinitializer
-\sa ESOS_TASK_WAIT_ON_READ1I2C1
-\sa ESOS_TASK_WAIT_ON_READNI2C1
-\sa esos_hwxxx_configI2C1
-*/
-#define   ESOS_TASK_WAIT_ON_READ2I2C1( u8_addr, u8_d1, u8_d2 )                     \
+ * Transaction: Read 2 (TWO) bytes from I2C slave at address \em u8_addr, save to variables \em u8_d1 and \em u8_d2
+ * As per the I2C standard, a NAK is returned for the last byte read from the slave, ACKs are returned for the other bytes.
+ * \param u8_addr  Slave I2C address
+ * \param u8_d1   variable to hold first read byte
+ * \param u8_d2   variable to hold second read byte
+ * \hideinitializer
+ * \sa ESOS_TASK_WAIT_ON_READ1I2C
+ * \sa ESOS_TASK_WAIT_ON_READNI2C
+ * \sa esos_hwxxx_configI2C1
+ */
+#define   ESOS_TASK_WAIT_ON_READ2I2C( u8_addr, u8_d1, u8_d2 )                     \
      do{                                                                              \
-       ESOS_TASK_SPAWN_AND_WAIT( (ESOS_TASK_HANDLE)&__stChildTaskI2C, __esos_hwxxx_readNI2C1, (u8_addr), &__esos_i2c_dataBytes[0], 2 );      \
+       ESOS_TASK_SPAWN_AND_WAIT( (ESOS_TASK_HANDLE)&__stChildTaskI2C, __esos_i2c_hw_readN, (u8_addr), &__esos_i2c_dataBytes[0], 2 );      \
        (u8_d1) = __esos_i2c_dataBytes[0];                                             \
        (u8_d2) = __esos_i2c_dataBytes[1];                                             \
      }while(0)
 
 /**
-Transaction: Read \em u16_cnt bytes from I2C slave at address \em u8_addr, save to buffer \em *pu8_d.
-As per the I2C standard, a NAK is returned for the last byte read from the slave, ACKs are returned for the other bytes.
-\param u8_addr  Slave I2C address
-\param pu8_d Pointer to buffer for storing bytes read from slave
-\param u16_cnt Number of bytes read from slave.
-\hideinitializer
-\sa ESOS_TASK_WAIT_ON_READ1I2C1
-\sa ESOS_TASK_WAIT_ON_READ2I2C1
-\sa esos_hwxxx_configI2C1
-*/
-#define   ESOS_TASK_WAIT_ON_READNI2C1( u8_addr, pu8_d, u16_cnt )                     \
-            ESOS_TASK_SPAWN_AND_WAIT( (ESOS_TASK_HANDLE)&__stChildTaskI2C, __esos_hwxxx_readNI2C1, (u8_addr), (pu8_d), (u16_cnt) )
-
-
+ * Transaction: Read \em u8_cnt bytes from I2C slave at address \em u8_addr, save to buffer \em *pu8_d.
+ * As per the I2C standard, a NAK is returned for the last byte read from the slave, ACKs are returned for the other bytes.
+ * \param u8_addr  Slave I2C address
+ * \param pu8_d Pointer to buffer for storing bytes read from slave
+ * \param u8_cnt Number of bytes read from slave.
+ * \hideinitializer
+ * \sa ESOS_TASK_WAIT_ON_READ1I2C
+ * \sa ESOS_TASK_WAIT_ON_READ2I2C
+ * \sa __esos_i2c_hw_configI2C1
+ *
+ **/
+#define   ESOS_TASK_WAIT_ON_READNI2C( u8_addr, pu8_d, u8_cnt )                     \
+            ESOS_TASK_SPAWN_AND_WAIT( (ESOS_TASK_HANDLE)&__stChildTaskI2C, __esos_i2c_hw_readNI2C, (u8_addr), (pu8_d), (u8_cnt) )
+ 
 /* P U B L I C  P R O T O T Y P E S *****************************************/
 
 //I2C Operations
-void esos_hwxxx_configI2C1(uint16_t u16_FkHZ);
+void __esos_i2c_hw_config(uint32_t u32_i2cbps);
 
 //I2C Transactions
-ESOS_CHILD_TASK( __esos_hwxxx_getI2C1, uint8_t* pu8_x, uint8_t u8_ack2Send);
-ESOS_CHILD_TASK( __esos_hwxxx_writeNI2C1, uint8_t u8_addr, uint8_t* pu8_d, uint16_t u16_cnt);
-ESOS_CHILD_TASK( __esos_hwxxx_readNI2C1, uint8_t u8_addr, uint8_t* pu8_d, uint16_t u16_cnt);
+ESOS_CHILD_TASK( __esos_i2c_hw_writeN, uint8_t u8_addr, uint8_t* pu8_d, uint_t u8_cnt);
+ESOS_CHILD_TASK( __esos_i2c_hw_readN, uint8_t u8_addr, uint8_t* pu8_d, uint8_t u8_cnt);
+ESOS_CHILD_TASK( __esos_i2c_hw_writeNReadM, uint8_t u8_addr, uint8_t* pu8_wd, uint8_t u8_wcnt, uint8_t* pu8_rd, uint8_t u8_rcnt);
+
 
 /** @} */
 #endif // end ESOS_HWXXX_I2C_H
--- src/generic_hw/esos_hwxxx_i2c.c
+++ src/generic_hw/esos_hwxxx_i2c.c
@@ -42,8 +42,11 @@
 /*** I N C L U D E S *************************************************/
 #include "esos_hwxxx_i2c.h"
 
+
+/*** D E F I N E S *************************************************/
+
 /*** G L O B A L S *************************************************/
-struct stTask   __stChildTaskI2C, __stGrandChildTaskI2C;
+struct stTask   	__stChildTaskI2C, __stGrandChildTaskI2C;
 uint8_t           __esos_i2c_dataBytes[2];
 
 /*** T H E   C O D E *************************************************/
@@ -57,94 +60,75 @@ uint8_t           __esos_i2c_dataBytes[2];
 // Note: place this comment below the #if NUM_I2C_MODS so Doxygen
 // will only see it once.
 /** \file
- *  I2C support functions. \see pic24_i2c.h for details.
+ *  I2C support functions. \see esos_i2c.h for details.
  */
 
 
 /**
-Configure and enable the I2C1 module for operation at \em u16_fKHz KHz clock speed.
-\param u16_fKHz specifies clock speed in KHz
+Configure and enable the I2C1 module for operation at \em u32_i2cbps.
+\param u32_i2cbps specifies clock speed in bits per second
 \sa ESOS_TASK_WAIT_ON_WRITENI2C1
 \sa ESOS_TASK_WAIT_ON_READNI2C1
 \hideinitializer
  */
-void esos_hwxxx_configI2C1(uint16_t u16_fKHz) {
-  uint32_t u32_temp;
+void __esos_i2c_hw_config(uint32_t u32_i2cbps) {
+	// Set up the hardware aspects of the STM32L4 interface of the
+	// I2C service direction, timings, address sizes, etc beyond
+	// what is already done in esos_i2c_config()
 
-  u32_temp = (FCY/1000L)/((uint32_t) u16_fKHz);
-  u32_temp = u32_temp - FCY/10000000L - 1;
-  I2C1BRG = u32_temp;
-  I2C1CONbits.I2CEN = 1;
+
 }
 
-ESOS_CHILD_TASK( __esos_hwxxx_getI2C1, uint8_t* pu8_x, uint8_t u8_ack2Send) {
-  static uint8_t        u8_tempAck;
-
+/**
+ * Transaction: Write \em u8_cnt bytes stored in buffer \em *pu8_d to I2C slave,
+ * I2C slave is located at I2C address \em u8_addr.
+ * \param u8_addr  Slave I2C address
+ * \param pu8_d Pointer to buffer containing bytes to send
+ * \param u8_cnt Number of bytes to send
+ * 
+ */
+ESOS_CHILD_TASK( __esos_i2c_hw_writeN, uint8_t u8_addr, uint8_t* pu8_d, uint8_t u8_cnt) {
   ESOS_TASK_BEGIN();
-  u8_tempAck = u8_ack2Send;
 
-  ESOS_TASK_WAIT_WHILE(I2C1CON & 0x1F);           //wait for idle condition
-  I2C1CONbits.RCEN = 1;                           //enable receive
-  ESOS_TASK_WAIT_UNTIL(I2C1STATbits.RBF);         //wait for receive byte
-  *pu8_x = I2C1RCV;                               //read byte;
-  //wait for idle condition before attempting ACK
-  ESOS_TASK_WAIT_WHILE(I2C1CON & 0x1F);           //lower 5 bits must be 0
-  I2C1CONbits.ACKDT = u8_tempAck;                 //ACK bit to send back on receive
-  I2C1CONbits.ACKEN = 1;                          //enable ACKbit transmittion
-  ESOS_TASK_WAIT_WHILE(I2C1CONbits.ACKEN);        //wait for completion
+	// put your hardware specific-code here
+  
   ESOS_TASK_END();
-} // end __esos_hwxxx_getI2C1
+} // end __esos_i2c_hw_writeN
 
 /**
-Transaction: Write \em u16_cnt bytes stored in buffer \em *pu8_d to I2C slave at address \em u8_addr.
-\param u8_addr  Slave I2C address
-\param pu8_d Pointer to buffer containing bytes to send
-\param u16_cnt Number of bytes to send
+ * Transaction: Read \em u8_cnt bytes from I2C slave at address
+ * \em u8_addr, save to buffer \em *pu8_d.
+ * As per the I2C standard, a NAK is returned for the last byte read
+ * from the slave, ACKs are returned for the other bytes.
+ * \param u8_addr  Slave I2C address
+ * \param pu8_d Pointer to buffer for storing bytes read from slave
+ * \param u8_cnt Number of bytes read from slave.
+ * 
  */
-ESOS_CHILD_TASK( __esos_hwxxx_writeNI2C1, uint8_t u8_addr, uint8_t* pu8_d, uint16_t u16_cnt) {
-  static uint8_t        u8_tempAddr;
-  static uint8_t*       pu8_tempPtr;
-  static uint16_t       u16_tempCnt, u16_i;
-
+ESOS_CHILD_TASK( __esos_i2c_hw_readN, uint8_t u8_addr, uint8_t* pu8_d, uint8_t u8_cnt) {
   ESOS_TASK_BEGIN();
-  u8_tempAddr=u8_addr;
-  pu8_tempPtr=pu8_d;
-  u16_tempCnt=u16_cnt;
-  __HWXXX_I2C1_START();
-  __HWXXX_I2C1_PUT(I2C_WADDR(u8_tempAddr));
-  for (u16_i=0; u16_i < u16_tempCnt; u16_i++) {
-    __HWXXX_I2C1_PUT(*pu8_tempPtr);
-    pu8_tempPtr++;
-  }
-  __HWXXX_I2C1_STOP();
-  ESOS_TASK_END();
-} // end __esos_hwxxx_writeNI2C1
 
+	// put your hardware specific-code here
+
+  ESOS_TASK_END();
+} // end __esos_i2c_hw_readN
+  
 /**
-Transaction: Read \em u16_cnt bytes from I2C slave at address \em u8_addr, save to buffer \em *pu8_d.
-As per the I2C standard, a NAK is returned for the last byte read from the slave, ACKs are returned for the other bytes.
-\param u8_addr  Slave I2C address
-\param pu8_d Pointer to buffer for storing bytes read from slave
-\param u16_cnt Number of bytes read from slave.
+ * 
+ * ESOS_CHILD_TASK( __esos_i2c_hw_writeMReadN, uint8_t u8_addr, uint8_t* pu8_wd, uint8_t u8_wcnt, uint8_t* pu8_rd, uint8_t u8_rcnt)
+ * 
+ * Transaction: Write \em u8_wcnt bytes stored in buffer \em *pu8_wd to I2C slave,
+ * then read \em u8_rcnt bytes into buffer \em *pu8_rd from I2C slave.
+ * I2C slave is located at I2C address \em u8_addr.
+ * \param u8_addr  Slave I2C address
+ * \param pu8_d Pointer to buffer containing bytes to send
+ * \param u16_cnt Number of bytes to send
+ * 
  */
-ESOS_CHILD_TASK( __esos_hwxxx_readNI2C1, uint8_t u8_addr, uint8_t* pu8_d, uint16_t u16_cnt) {
-  static uint8_t        u8_tempAddr;
-  static uint8_t*       pu8_tempD;
-  static uint16_t       u16_tempCnt, u16_i;
-
+ESOS_CHILD_TASK( __esos_i2c_hw_writeNReadM, uint8_t u8_addr, uint8_t* pu8_wd, uint8_t u8_wcnt, uint8_t* pu8_rd, uint8_t u8_rcnt) {
   ESOS_TASK_BEGIN();
-  u8_tempAddr=u8_addr;
-  pu8_tempD=pu8_d;
-  u16_tempCnt=u16_cnt;
-  __HWXXX_I2C1_START();
-  __HWXXX_I2C1_PUT(I2C_RADDR(u8_tempAddr));
-  for (u16_i=0; u16_i < u16_tempCnt-1; u16_i++) {
-    ESOS_TASK_WAIT_ON_GETI2C1(pu8_tempD, I2C_ACK);
-    pu8_tempD++;
-  }
-  ESOS_TASK_WAIT_ON_GETI2C1(pu8_tempD, I2C_NAK);
-  __HWXXX_I2C1_STOP();
+  
+	// put your hardware specific-code here  
+  
   ESOS_TASK_END();
-  /** @} */
-} // end __esos_hwxxx_readNI2C1
-
+} // end __esos_i2c_hw_writeN
