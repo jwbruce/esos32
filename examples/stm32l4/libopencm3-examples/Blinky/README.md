5cec258e299e58e26860afd054d285d9b94505ed
Jackie <jmmcwhorte43@students.tntech.edu>
2020-01-26 10:33:53 -0600

    This is the code necessary to get LED2 to blink on the Nucleo-L452RE development board using libopencm3. Additionally, the makefile creates ASM (.S) files. This does not use any ESOS systems. See readme.md

---
+++ examples/stm32l4/libopencm3-examples/Blinky/README.md
@@ -0,0 +1,21 @@
+This code does not use the ESOS32 system in any regard. Additionally, the makefile generates ASM files with .S extensions.
+
+**Steps to running blinky_blink**
+------------------------------------
+    -Within the Makefile, change directories to match the locations to which your directories are
+    -Open a cmd prompt within the folder with this makefile
+    -Run mingw32-make to generate hex file
+    -Use STMCubeProgrammer to upload hex file to board
+
+/**Note: this example can serve as a foundation for labs. The only modifications that need to be changed are as follows**/
+    -Change the target to match that of your file with your main loop
+    -In C-source, add all files that are found in you ./src/ folder 
+    -When ESOS is added in, add the ESOS files to the C-source section of the makefile in the format:
+     (ESOS_DIR)/src/blahblahblah.c which needs to be added for each of the esos src files.
+    -You will need to add a ESOS_DIR that points to the esos directory to the top of the makefile
+     /**You will need to add the include and lib as well, in the C-include section. Follow the CM3 setup, or ESOSCM3 makefiles in armdev**/
+     
+/** THESE MAKEFILES WILL NOT COMPILE IN MSYS. MSYS IS NECESSARY TO COMPILE ANYTHING WITH LIBOPENCM. USE MINGW32-MAKE FOR ALL OTHER CASES THAT DO NOT INVOLVE A LIBOPENCM3 MAKEFILE**/
+
+
+     
---
+++ examples/stm32l4/libopencm3-examples/Blinky/src/user_app.h
@@ -0,0 +1,63 @@
+/*
+ * "Copyright (c) 2019 J. W. Bruce ("AUTHOR(S)")"
+ * All rights reserved.
+ * (J. W. Bruce, jwbruce_AT_tntech.edu, Tennessee Tech University)
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose, without fee, and without written agreement is
+ * hereby granted, provided that the above copyright notice, the following
+ * two paragraphs and the authors appear in all copies of this software.
+ *
+ * IN NO EVENT SHALL THE "AUTHORS" BE LIABLE TO ANY PARTY FOR
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+ * OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE "AUTHORS"
+ * HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THE "AUTHORS" SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
+ * ON AN "AS IS" BASIS, AND THE "AUTHORS" HAS NO OBLIGATION TO
+ * PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS."
+ *
+ * Please maintain this header in its entirety when copying/modifying
+ * these files.
+ *
+ *
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __USER_APP_H
+#define __USER_APP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+
+
+/* Private includes ----------------------------------------------------------*/
+
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported constants --------------------------------------------------------*/
+
+/* Exported macro ------------------------------------------------------------*/
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* Private defines -----------------------------------------------------------*/
+
+#define LED2_Pin GPIO5
+#define LED2_GPIO_Port GPIOA
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __USER_APP_H */
